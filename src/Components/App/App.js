c7e333b4f6afcb58deaff661f8db3f59f9251167
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-06 21:28:18 +0300

    added  input and function for POST request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,71 @@
-import './App.css';
-import { Input } from '../Input/Input';
+import React from "react";
+import "./App.css";
+import { Input } from "../Input/Input";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enteredLink: "",
+      shortedLink: "",
+    };
+
+    this.enterLink = this.enterLink.bind(this);
+    this.makeShortLink = this.makeShortLink.bind(this);
+  }
+
+  enterLink(newLink) {
+    this.setState({
+      enteredLink: newLink,
+    });
+  }
+
+  makeShortLink() {
+    const api = "APIKEY";
+    const url = "https://api.rebrandly.com/v1/links";
+    const data = JSON.stringify({ destination: this.state.enteredLink });
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        'apikey': api
+      },
+      body: data,
+    })
+      .then(
+        (response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Request failed!");
+        },
+        (error) => {
+          console.log(error.message);
+        }
+      )
+      .then((jsonResponse) => {
+        this.setState({
+          shortedLink: jsonResponse["shortUrl"],
+        });
+      });
+  }
+
+  render() {
     return (
       <div className="container">
         <h1>Put your link here: </h1>
-        <Input />
+        <Input
+          enteredLink={this.state.enteredLink}
+          setLink={this.enterLink}
+          shortLink={this.makeShortLink}
+        />
+        <h1>{this.state.shortedLink}</h1>
+        <h1>{this.state.enteredLink}</h1>
       </div>
     );
+  }
 }
 
 export default App;
--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,6 +1,36 @@
-import React from 'react';
-import './Input.css';
+import React from "react";
+import "./Input.css";
 
 export class Input extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.shortLink();
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    this.props.setLink(value);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.props.enteredLink}
+          />
+          <button>CLICK</button>
+        </form>
+      </div>
+    );
+  }
 }
